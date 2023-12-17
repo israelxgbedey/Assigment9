7554f111126b88f4634b251175e715bcbdb226ed
israelxgbedey <88339733+israelxgbedey@users.noreply.github.com>
2023-12-16 23:39:42 -0600

    Final commit

---
+++ HttpClientHelper.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RESTfulWebApi
+{
+    public class HttpClientHelper
+    {
+        private readonly HttpClient client;
+
+        public HttpClientHelper(string baseUrl)
+        {
+            client = new HttpClient { BaseAddress = new Uri(baseUrl) };
+        }
+
+        public async Task AddUser(User user)
+        {
+            var json = JsonConvert.SerializeObject(user);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("api/user", content);
+
+            HandleResponse(response);
+        }
+
+        public async Task RemoveUser(int userId)
+        {
+            var response = await client.DeleteAsync($"api/user/{userId}");
+
+            HandleResponse(response);
+        }
+
+        public async Task GetUser(int userId)
+        {
+            var response = await client.GetAsync($"api/user/{userId}");
+
+            HandleResponse(response);
+        }
+
+        private void HandleResponse(HttpResponseMessage response)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,40 @@ class Program
     {
         string apiUrl = "http://ec2-18-223-122-221.us-east-2.compute.amazonaws.com:5143/api/user";
 
+        // Add a user
+        await AddUser(apiUrl);
+
+        // Display the users
+        await DisplayUsers(apiUrl);
+
+        // Prompt the user for the ID to delete
+        Console.WriteLine("Do you want to delete a user? (Y/N)");
+        string deleteOption = Console.ReadLine().ToUpper();
+
+        if (deleteOption == "Y")
+        {
+            Console.WriteLine("Enter the ID of the user to delete:");
+            if (int.TryParse(Console.ReadLine(), out int userIdToDelete))
+            {
+                // Delete the user with the specified ID
+                await DeleteUser(apiUrl, userIdToDelete);
+
+                // Display the updated users list
+                await DisplayUsers(apiUrl);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid numeric ID.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Delete operation skipped.");
+        }
+    }
+
+    static async Task AddUser(string apiUrl)
+    {
         Console.WriteLine("Please enter your name:");
         string userName = Console.ReadLine();
 
@@ -20,13 +54,63 @@ class Program
 
         using (HttpClient client = new HttpClient())
         {
-            // Convert data to JSON and then to content
             var json = JsonConvert.SerializeObject(userData);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             try
             {
                 HttpResponseMessage response = await client.PostAsync(apiUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+    static async Task DisplayUsers(string apiUrl)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Current Users:");
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+    static async Task DeleteUser(string apiUrl, int userId)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.DeleteAsync($"{apiUrl}/{userId}");
 
                 if (response.IsSuccessStatusCode)
                 {
